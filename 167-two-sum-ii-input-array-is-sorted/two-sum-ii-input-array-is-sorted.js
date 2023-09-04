74dfa75f1a6d147b87946869a63c698b4f5a9faf
Akash Yadav <akashrequired@gmail.com>
2023-09-04 22:21:11 +0530

    Time: 56 ms (68.47%) | Memory: 43 MB (27.52%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -6,24 +6,23 @@
 var twoSum = function(numbers, target) {
 
     //* M-1
-    // let [left, right] = [0, numbers.length - 1];
-    // while (left < right) {
-    //     const sum = numbers[left] + numbers[right];
-
-    //     if (sum === target) return [left + 1, right + 1];
-    //     if (sum < target) left++;
-    //     if (sum>target) right--;
-    // }
-    // return [-1, -1];
+    let [left, right] = [0, numbers.length - 1];
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        if (sum === target) return [left + 1, right + 1];
+        if (sum < target) left++;
+        if (sum>target) right--;
+    }
+    return null;
 
     //* M-2
-    for (let i = 0; i < numbers.length - 1; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === target) {
-              return [i + 1, j + 1]; // Adjust indices to be 1-based
-         }
-      }
-    }
-    return null; // If no solution is found
+    // for (let i = 0; i < numbers.length - 1; i++) {
+    //     for (let j = i + 1; j < numbers.length; j++) {
+    //         if (numbers[i] + numbers[j] === target) {
+    //           return [i + 1, j + 1]; // Adjust indices to be 1-based
+    //      }
+    //   }
+    // }
+    // return null; // If no solution is found
 
 };
