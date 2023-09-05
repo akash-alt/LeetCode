ec52d1106637ef57449a44f429a729c0230ed928
Akash Yadav <akashrequired@gmail.com>
2023-09-05 22:40:12 +0530

    Time: 64 ms (78.32%) | Memory: 49 MB (95.87%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -9,7 +9,6 @@ var maxArea = function(height) {
 
     while(left < right){
         let width = (right-left);
-
         let maxArea = Math.min(height[left], height[right])*width;
         maxima = Math.max(maxima,maxArea);
 
@@ -20,4 +19,21 @@ var maxArea = function(height) {
         }
     }
     return maxima;
+
+    //******************************************** *//
+    //* M-2
+    // let maxima = 0;
+    // // Iterate through all possible pairs of lines
+    // for (let i = 0; i < height.length - 1; i++) {
+    //     for (let j = i + 1; j < height.length; j++) {
+    //         // Calculate the width (distance between the lines)
+    //         let width = j - i;
+    //         // Calculate the minimum height between the two lines
+    //         let minHeight = Math.min(height[i], height[j]);
+    //         // Calculate the area and update maxima if it's greater
+    //         let area = width * minHeight;
+    //         maxima = Math.max(maxima, area);
+    //     }
+    // }
+    // return maxima;
 };
