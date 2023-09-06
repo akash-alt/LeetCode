c07ebbe8a8f4b4cf2b9207fa8234e50248e89f3b
Akash Yadav <akashrequired@gmail.com>
2023-09-06 11:03:55 +0530

    Time: 51 ms (93.29%) | Memory: 43.2 MB (50.38%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+
+    //* Solution is good
+    // let trappedWater = 0;
+    // for (let i = 0; i < height.length; i++) {
+    //     let leftMax = 0;
+    //     let rightMax = 0;
+    //     // Find the maximum height to the left of the current position
+    //     for (let j = i; j >= 0; j--) {
+    //         leftMax = Math.max(leftMax, height[j]);
+    //     }
+    //     // Find the maximum height to the right of the current position
+    //     for (let k = i; k < height.length; k++) {
+    //         rightMax = Math.max(rightMax, height[k]);
+    //     }
+    //     // Calculate the amount of trapped water at the current position
+    //     trappedWater += Math.min(leftMax, rightMax) - height[i];
+    // }
+    // return trappedWater;
+
+
+    //*********************************************************** *//
+    let i=0, j=height.length-1,maxLeft=0,maxRight=0;
+    let totalTrapWater=0;
+    while(i<j){
+        if(height[i]<=height[j]){
+            maxLeft=Math.max(maxLeft,height[i])
+            totalTrapWater+=maxLeft-height[i];
+            i++;
+        }
+         else{
+            maxRight=Math.max(maxRight,height[j])
+            totalTrapWater+=maxRight-height[j];
+            j--;
+        }
+    }
+    return totalTrapWater;
+
+};
