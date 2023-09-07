3735e3715742426ddc2aec3d4c4e0022364ff746
Akash Yadav <akashrequired@gmail.com>
2023-09-07 22:00:35 +0530

    Time: 70 ms (88.03%) | Memory: 50 MB (85.66%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -4,6 +4,35 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
+//    let count = 0;
+//    for(let i=0;i<nums.length;i++){
+//        let sum = 0 ;
+//        for(let j=i;j<nums.length;j++){
+//            sum = sum+nums[j];
+//            if(sum === k){
+//                return count += 1;
+//            }
+//        }
+//    }
+//    return count;
+
+//*
+
+    // let count = 0;
+    // const map = new Map([[0,1]]);
+    // let sum = 0;
+
+    // for(let i=0;i<nums.length;i++){
+    //     sum += nums[i];
+    //     if(map.has[sum-k]){
+    //         count += map.get(sum-k)
+    //     }
+    //     map.set(sum,(map.get(sum) || 0)+1);
+
+    // }
+    // return count;
+
+    //*********************************** *//
     let count = 0;
     let currentSum = 0;
     let i = 0;
