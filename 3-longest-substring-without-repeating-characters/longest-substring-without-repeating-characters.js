ddf73e5ba0811de10e0acc51407920caa1fb7ad1
Akash Yadav <akashrequired@gmail.com>
2023-09-10 22:49:07 +0530

    Time: 85 ms (51.62%) | Memory: 46.9 MB (68.59%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+   const set = new Set();
+    let left = 0;
+    let max = 0;
+
+for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+        set.delete(s[left]);
+        left++;
+    }
+    set.add(s[right]);
+    max = Math.max(max, set.size);
+}
+
+return max;
+     
+};
