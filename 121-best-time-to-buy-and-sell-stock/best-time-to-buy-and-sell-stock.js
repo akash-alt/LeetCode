3ad03ea597758d1a6f23f4f0b64491d6108dc2cd
Akash Yadav <akashrequired@gmail.com>
2023-09-10 20:37:13 +0530

    Time: 79 ms (38.88%) | Memory: 51.4 MB (69.39%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -21,19 +21,30 @@ var maxProfit = function(prices) {
 
 //* M-2
 
-    let minstockPurchasePrice = prices[0] 
-    prices[0] = 0
-    let profit = 0;
-    for(let i=1;i<prices.length;i++){
-        if(minstockPurchasePrice > prices[i]){
-            minstockPurchasePrice = prices[i]
-            prices[i] = 0
-        }
-        profit = Math.max(prices[i] - minstockPurchasePrice,profit)
-    }
-    return profit;
+    // let minstockPurchasePrice = prices[0] 
+    // prices[0] = 0
+    // let profit = 0;
+    // for(let i=1;i<prices.length;i++){
+    //     if(minstockPurchasePrice > prices[i]){
+    //         minstockPurchasePrice = prices[i]
+    //         prices[i] = 0
+    //     }
+    //     profit = Math.max(prices[i] - minstockPurchasePrice,profit)
+    // }
+    // return profit;
 
-   
+//* M-3
+ let [left, right, max] = [0, 1, 0];
+    while (right < prices.length) {
+        const canSlide = prices[right] <= prices[left];
+        if (canSlide) left = right;
+
+        const window = prices[right] - prices[left];
+
+        max = Math.max(max, window);
+        right++;
+    }
+    return max;
 };
 
 // maxProfit([7,1,5,3,6,4])
