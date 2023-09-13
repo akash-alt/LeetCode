dd928bd9afe19da104c167fd74672c7e7f88de53
Akash Yadav <akashrequired@gmail.com>
2023-09-13 17:48:35 +0530

    Time: 53 ms (75.45%) | Memory: 42.3 MB (66.82%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -12,31 +12,48 @@ var isValid = function(s) {
     // }
     // return false;
 
-let stack = [];
-let parentheses = "([{";
-let closingParentheses = ")]}";
+//* M-2
 
-for (let i = 0; i < s.length; i++) {
-  let char = s[i];
+// let stack = [];
+// let parentheses = "([{";
+// let closingParentheses = ")]}";
 
-  if (parentheses.includes(char)) {
-    stack.push(char);
-  } else if (closingParentheses.includes(char)) {
-    if (stack.length === 0) {
-      return false; // No matching opening parenthesis found
+// for (let i = 0; i < s.length; i++) {
+//   let char = s[i];
+
+//   if (parentheses.includes(char)) {
+//     stack.push(char);
+//   } else if (closingParentheses.includes(char)) {
+//     if (stack.length === 0) {
+//       return false; // No matching opening parenthesis found
+//     }
+
+//     let top = stack.pop();
+//     if (
+//       (char === ")" && top !== "(") ||
+//       (char === "}" && top !== "{") ||
+//       (char === "]" && top !== "[")
+//     ) {
+//       return false; // Mismatched parentheses
+//     }
+//   }
+// }
+// return stack.length === 0; // Check if all parentheses are balanced
+
+//* M-3
+ s = s.split("");
+    var stack = [];
+    for (var z of s) {
+        if (z === "(" || z === "{" || z === "[") {
+            stack.push(z);
+        } else {
+            var top = stack[stack.length - 1];
+            if (z === ")" && top === "(" || z === "]" && top === "[" || z === "}" && top === "{")
+                stack.pop();
+            else
+                return false;
+        }
     }
-
-    let top = stack.pop();
-    if (
-      (char === ")" && top !== "(") ||
-      (char === "}" && top !== "{") ||
-      (char === "]" && top !== "[")
-    ) {
-      return false; // Mismatched parentheses
-    }
-  }
-}
-
-return stack.length === 0; // Check if all parentheses are balanced
+    return stack.length === 0;
 
 };
