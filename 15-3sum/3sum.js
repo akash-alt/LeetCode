ae49673b62f07a113d22c1f8121eb85185a3bb54
Akash Yadav <akashrequired@gmail.com>
2023-10-05 23:01:57 +0530

    Time: 159 ms (44.65%) | Memory: 58.5 MB (77.93%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -2,49 +2,36 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+
 var threeSum = function(nums) {
-//   const result = [];
-//   // Sort the input array
-//   nums.sort((a, b) => a - b);
-//   for (let i = 0; i < nums.length - 2; i++) {
-//     for (let j = i + 1; j < nums.length - 1; j++) {
-//       for (let k = j + 1; k < nums.length; k++) {
-//         if (nums[i] + nums[j] + nums[k] === 0) {
-//           result.push([nums[i], nums[j], nums[k]]);
-//         }
-//       }
-//     }
-//   }
-//   return result;
+    if (nums.length === 0) return [];
+    nums = nums.sort((a, b) => a - b);
 
-//**************************************** *//
-// var threeSum = function(nums) {
-  let res = [];
-  nums.sort((a, b) => a - b);
+    let res = [];
+    for (let i = 0; i < nums.length - 2; i++) {
+        // Checking for duplicate occurrences
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
 
-  for (let i = 0; i < nums.length; i++) {
-    let left = i + 1;
-    let right = nums.length - 1;
+        let j = i + 1;
+        let k = nums.length - 1;
 
-    while (left < right) {
-      let sum = nums[left] + nums[right];
+        while (j < k) {
+            let sum = nums[i] + nums[j] + nums[k];
+            
+            if (sum === 0) {
+                res.push([nums[i], nums[j], nums[k]]);
+            //* stop duplicates
+                while (nums[j] === nums[j + 1]) j++;
+                while (nums[k] === nums[k - 1]) k--;
 
-      if (sum > -nums[i]) {
-        right--;
-      } else if (sum < -nums[i]) {
-        left++;
-      } else {
-        res.push([nums[i], nums[left], nums[right]]);
-        while (nums[left] == nums[left + 1]) left++;
-        while (nums[right] == nums[right - 1]) right--;
-        right--;
-        left++;
-      }
+                j++;
+                k--;
+            } else if (sum < 0) {
+                j++;
+            } else {
+                k--;
+            }
+        }
     }
-
-    while (nums[i] == nums[i + 1]) i++;
-  }
-
-  return res;
+    return res;
 };
-
