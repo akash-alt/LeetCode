19e261a65ce2357b5754a6d2aea7dd53b6a151f5
Akash Yadav <akashrequired@gmail.com>
2024-01-04 18:09:08 +0530

    Time: 74 ms (55.54%) | Memory: 45.1 MB (58.39%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.js
@@ -0,0 +1,40 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+var numIslands = function(grid) {
+    if (!grid || grid.length === 0) {
+        return 0;
+    }
+
+    const rows = grid.length;
+    const cols = grid[0].length;
+
+    const dfs = (row, col) => {
+        if (row < 0 || row >= rows || col < 0 || col >= cols || grid[row][col] === '0') {
+            return;
+        }
+
+        grid[row][col] = '0'; // Mark the current cell as visited
+
+        // Explore adjacent cells
+        dfs(row + 1, col);
+        dfs(row - 1, col);
+        dfs(row, col + 1);
+        dfs(row, col - 1);
+    };
+
+    let islandCount = 0;
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j] === '1') {
+                islandCount++;
+                dfs(i, j);
+            }
+        }
+    }
+
+    return islandCount;
+};
