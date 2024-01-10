5ff493787ccc5ea5845be54add9de151c4795b4b
Akash Yadav <akashrequired@gmail.com>
2024-01-10 19:43:00 +0530

    Time: 54 ms (45.59%) | Memory: 41.9 MB (55.25%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let filteredArr = [];
+    let result = [];
     for(let i=0;i<arr.length;i++){
         if(fn(arr[i],i,arr)){
-            filteredArr.push(arr[i])
+            result.push(arr[i])
         }
     }
-    return filteredArr
+    return result
 };
